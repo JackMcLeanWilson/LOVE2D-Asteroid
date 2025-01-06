ed74c796bd1139e6afef62555fcaaaa2bc931ee8
QuarkInComputing <jackmcleanwilson@gmail.com>
2025-01-06 20:49:07 +0000

    Created laser.lua

---
+++ game/laser.lua
@@ -0,0 +1,22 @@
+local Laser = {}
+
+function Laser:load()
+
+	--Limits
+	self.Lifespan = 5
+
+	-- Position
+
+	--Sprites
+	self.SpriteLaser("assets/sprites/laser.png")
+end
+
+function Laser:Update(dt)
+	-- Move Laser
+
+	
+end
+
+function Laser:draw()
+
+end
